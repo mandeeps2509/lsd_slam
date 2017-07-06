81b1b56bd3f61cb4d25b0730b9de9b87ed63eff6
Mandeep Singh <Mandeep Singh>
2017-07-07 01:40:17 +0530

    Fixed Eigen version problem (establish soft link to eigen 3.2.5-4)

--- lsd_slam_core/src/util/settings.cpp
+++ lsd_slam_core/src/util/settings.cpp
@@ -35,7 +35,7 @@ bool autoRunWithinFrame = true;
 int debugDisplay = 0;
 
 bool onSceenInfoDisplay = true;
-bool displayDepthMap = true;
+bool displayDepthMap = false;	//as stated in issue 224
 bool dumpMap = false;
 bool doFullReConstraintTrack = false;
 
